9defdfa0f6b05c4ee0a7e125aa67001e186cab70
Jan <olea.janrobert@gmail.com>
2022-04-25 20:35:35 +0800

    initial design for navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
 import 'package:flutter/material.dart';
+
+import 'navigation/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,57 +14,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Nunito",
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        body: Container(
+          color: const Color(0xFFB1F2B36),
+          child: const Navbar(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/navigation/navbar.dart
@@ -0,0 +1,136 @@
+
+import 'package:flutter/material.dart';
+
+import 'menu_item.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({Key? key}) : super(key: key);
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  final List<String> menuItems = [
+    'Dashboard',
+    'Assets',
+    'Expenses',
+    'Settings'
+  ];
+  final List<String> menuIcons = [
+    'assets/images/assets.png',
+    'assets/images/assets.png',
+    'assets/images/expenses.png',
+    'assets/images/expenses.png',
+  ];
+
+  bool sidebarOpen = false;
+
+  double xOffset = 60;
+  double yOffset = 0;
+
+  void setSidebarState() {
+    setState(() {
+      xOffset = sidebarOpen ? 265 : 60;
+    });
+  }
+
+  int selectedMenuItem = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 24),
+                // ignore: use_full_hex_values_for_flutter_colors
+                color: const Color(0xFFB1F2B36),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(20.0),
+                      child: const Text('LG')
+                      // child: Image.asset('assets/images/${menuIcons[index]}.png'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        'Logo Here',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: menuItems.length,
+                  itemBuilder: (context, index) => GestureDetector(
+                    onTap: () {
+                      sidebarOpen = false;
+                      selectedMenuItem = index;
+                      setSidebarState();
+                    },
+                    child: MenuItem(
+                      itemIcon: menuIcons[index],
+                      itemText: menuItems[index],
+                      selected: selectedMenuItem,
+                      position: index,
+                    ),
+                  ),
+                ),
+              ),
+              MenuItem(
+                itemIcon: menuIcons[1],
+                itemText: 'Logout',
+                selected: selectedMenuItem,
+                position: menuItems.length,
+              ),
+            ],
+          ),
+        ),
+        AnimatedContainer(
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 130),
+          transform: Matrix4.translationValues(xOffset, yOffset, 1.0),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 24),
+                  height: 60,
+                  child: Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          sidebarOpen = !sidebarOpen;
+                          setSidebarState();
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(20),
+                          child: const Icon(Icons.menu),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
