a6113f5a48010e68608bccf9b76588af4b91940d
Jan <olea.janrobert@gmail.com>
2022-04-29 21:28:08 +0800

    delete unused code

--- lib/navigation/navigation_block.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:project_a/navigation/navigation_event.dart';
-import 'package:project_a/navigation/navigation_state.dart';
-
-class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
-  NavigationBloc(NavigationState initialState) : super(NavigationStateDashboard());
-
-}
